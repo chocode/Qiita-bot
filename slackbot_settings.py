214896a23fd389bb9ac933c3707bcb6919fe8b76
S-N-33 <pokkycode@gmail.com>
2018-08-15 20:28:20 +0900

    slackbot_settingsの追加

---
+++ slackbot_settings.py
@@ -0,0 +1,15 @@
+﻿import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+env_path = join(dirname(__file__), '.env')
+load_dotenv(env_path)
+
+# botアカウントのトークンを指定
+API_TOKEN= os.environ.get("API_TOKEN")
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "どうしたら良いか分かりません"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
